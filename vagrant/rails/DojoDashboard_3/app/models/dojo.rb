6a3ede3977c4a7f8ec3d5c49bf0228b3d0dbb2ba
Garrett Bass <garrettbass@macbook-pro.attlocal.net>
2020-08-12 10:13:26 -0500

    DojoDash3 completed

---
+++ vagrant/rails/DojoDashboard_3/app/models/dojo.rb
@@ -0,0 +1,6 @@
+class Dojo < ActiveRecord::Base
+    validates :branch, presence: true, length: {minimum: 3}
+    validates :street, presence: true, length: {minimum: 3}
+    validates :city, presence: true, length: {minimum: 3}
+    validates :state, presence: true, length: {minimum:2, maximum: 2}
+end
