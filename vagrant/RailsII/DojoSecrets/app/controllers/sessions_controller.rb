1c03f582eaa9c85f4aed4d7ea5dac7427585aeee
Garrett Bass <garrettbass@macbook-pro.attlocal.net>
2020-08-18 16:07:25 -0500

    Authentication 1 complete

---
+++ vagrant/RailsII/DojoSecrets/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def login
+    @user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
+    unless @user
+      flash[:errors] = "Invalid Combination"
+      redirect_to "/sessions/new"
+    else 
+      redirect_to "/users/#{@user.id}"
+    end
+  end
+end
